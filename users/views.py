79efebb9d8e134c42b33236b6df00b9abcb98f69
Gabriele <gabriele.fabbri@edu.unifi.it>
2026-05-31 23:30:11 +0200

    Aggiunta pagina profilo utente con view, url e template di base

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def profile_view(request, username):
+    user = get_object_or_404(User, username=username)
+
+    profile = user.profile
+
+    context = {
+        'profile_user': user,
+        'profile': profile,
+    }
+    return render(request, 'users/profile.html', context)
