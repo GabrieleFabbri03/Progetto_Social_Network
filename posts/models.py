a60be75a4adba8d8051cbcb6fdc4ab5f796808fd
Gabriele <gabriele.fabbri@edu.unifi.it>
2026-05-31 21:08:10 +0200

    Creazione di modelli base per utenti e post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # Chi ha scritto il post (Collegamento all'utente)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    # Contenuto del post
+    testo = models.TextField(max_length=1000)
+
+    # Data di creazione
+    data_creazione = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username} - {self.data_creazione.strftime('%d/%m/%Y')}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    # Collega questo profilo a un Utente in modo 1 a 1
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Campi extra
+    bio = models.TextField(max_length=500, blank=True)
+
+    # Campo per i follow Molti a molti
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
